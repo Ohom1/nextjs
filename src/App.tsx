d9069799d03dec48bad413303f77635e01102907
Om <109742341+Ohom1@users.noreply.github.com>
2025-09-04 13:49:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,8 @@ const App = () => (
           <Route path="/sandbox" element={<Sandbox />} />
           <Route path="/compliance" element={<Compliance />} />
           <Route path="pages/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-of-service" element={<TermsOfService />} />
-          <Route path="/refund-policy" element={<RefundPolicy />} />
+          <Route path="pages/terms-of-service" element={<TermsOfService />} />
+          <Route path="pages/refund-policy" element={<RefundPolicy />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
